232cc480bb40cad5fb7ab3aec268c0eaadd0f57b
joke111 <alta.second@gmail.com>
2017-04-15 03:01:23 +0900

    상세 내용 대략 채움

--- app/static/food-map/js/side-slide.js
+++ app/static/food-map/js/side-slide.js
@@ -1,9 +1,9 @@
 function openSideSlide() {
     document.getElementById("restaurantDetail").style.width = "30%";
-    document.getElementById("map").style.width = "70%";
+//    document.getElementById("map").style.width = "70%";
 }
 
 function closeSideSlide() {
     document.getElementById("restaurantDetail").style.width = "0";
-    document.getElementById("map").style.width= "100%";
+//    document.getElementById("map").style.width= "100%";
 }
