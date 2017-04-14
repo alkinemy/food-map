2429cf78ef976e8e73cd45216104d43ab5913aeb
joke111 <alta.second@gmail.com>
2017-04-15 03:56:05 +0900

    정보 출력

--- app/restaurant/views.py
+++ app/restaurant/views.py
@@ -1,7 +1,7 @@
 from . import food_restaurant
 from .models import Restaurant, Comment
 
-from flask import redirect, request
+from flask import render_template, redirect, request
 
 
 @food_restaurant.route('/restaurants/<restaurant_id>/feedback/good', methods=['POST'])
@@ -17,6 +17,11 @@ def give_bad_feedback(restaurant_id):
 	Restaurant.objects(id=restaurant_id).update_one(inc__bad=1)
 	return redirect('/')
 
+
+@food_restaurant.route('/restaurants/<restaurant_id>')
+def detail(restaurant_id):
+	restaurant = Restaurant.objects.get(id=restaurant_id)
+	return render_template('restaurant-detail.html', restaurant=restaurant)
 
 
 
