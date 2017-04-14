526fe9d937b73be8baa93df1af6935cdd8061cd3
joke111 <alta.second@gmail.com>
2017-04-15 05:08:39 +0900

    toast 띄우기

--- app/static/food-map/js/map.js
+++ app/static/food-map/js/map.js
@@ -6,9 +6,9 @@ $('#restaurantDetailContent').on('click', '#giveGoodFeedback', function(event) {
         url: '/restaurants/' + $(this).data('restaurant-id') + '/feedback/good'
     }).done(function(count) {
         $("#goodFeedbackCount").html(count);
-        //TODO toast 보여주기
+        toastr.success('추천하였습니다!', '감사합니다!', {"positionClass": "toast-bottom-center"});
     }).fail(function() {
-        //TODO toast 보여주기
+        toastr.error('개발자에게 연락주세요 흑흑', '실패했습니다!', {"positionClass": "toast-bottom-center"})
     });
 });
 
@@ -20,8 +20,8 @@ $('#restaurantDetailContent').on('click', '#giveBadFeedback', function(event) {
         url: '/restaurants/' + $(this).data('restaurant-id') + '/feedback/bad'
     }).done(function(count) {
         $("#badFeedbackCount").html(count);
-        //TODO toast 보여주기
+        toastr.warning('비추천하였습니다!', '감사합니다!', {"positionClass": "toast-bottom-center"});
     }).fail(function() {
-        //TODO toast 보여주기
+        toastr.error('개발자에게 연락주세요 흑흑', '실패했습니다!', {"positionClass": "toast-bottom-center"})
     });
 });
