78c46a8b29925d2c3bd098e3b31c1f8e8507a11c
joke111 <alta.second@gmail.com>
2017-04-15 21:19:44 +0900

    댓글 뷰 추가

--- app/static/food-map/js/map.js
+++ app/static/food-map/js/map.js
@@ -4,8 +4,8 @@ $('#restaurantDetailContent').on('click', '#giveGoodFeedback', function(event) {
     $.ajax({
         method: 'POST',
         url: '/restaurants/' + $(this).data('restaurant-id') + '/feedback/good'
-    }).done(function(count) {
-        $("#goodFeedbackCount").html(count);
+    }).done(function(response) {
+        $("#goodFeedbackCount").html(response.good);
         toastr.success('추천하였습니다!', '감사합니다!', {"positionClass": "toast-bottom-center"});
     }).fail(function() {
         toastr.error('개발자에게 연락주세요 흑흑', '실패했습니다!', {"positionClass": "toast-bottom-center"})
@@ -18,8 +18,8 @@ $('#restaurantDetailContent').on('click', '#giveBadFeedback', function(event) {
     $.ajax({
         method: 'POST',
         url: '/restaurants/' + $(this).data('restaurant-id') + '/feedback/bad'
-    }).done(function(count) {
-        $("#badFeedbackCount").html(count);
+    }).done(function(response) {
+        $("#badFeedbackCount").html(response.bad);
         toastr.warning('비추천하였습니다!', '감사합니다!', {"positionClass": "toast-bottom-center"});
     }).fail(function() {
         toastr.error('개발자에게 연락주세요 흑흑', '실패했습니다!', {"positionClass": "toast-bottom-center"})
